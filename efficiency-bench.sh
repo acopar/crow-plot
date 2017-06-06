f624e819457230f4f5f103fd708a8809a8982865
acopar <andrej.copar@zoho.com>
2017-06-06 13:29:38 +0200

    [FIX] code cleanup

--- efficiency-bench.sh
+++ efficiency-bench.sh
@@ -2,4 +2,4 @@
 
 python crowpl/benchmark.py -c -u ArrayExpress TCGA-BRCA fetus retina cochlea
 scripts/crow-plot python /app/crowpl -a efficiency ArrayExpress TCGA-BRCA fetus retina cochlea
-scripts/crow-plot python /app/crowpl -a one ArrayExpress TCGA-BRCA fetus retina cochlea
+scripts/crow-plot python /app/crowpl -a transfer ArrayExpress TCGA-BRCA fetus retina cochlea
